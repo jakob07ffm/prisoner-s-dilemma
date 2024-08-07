75e19f698a44a50d7ce1c11a5134e87a55658d2a
Jakob__init__ <jsuetfels@gmail.com>
2024-08-07 22:19:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,17 @@ player_2_color = WHITE
 player_1_rects = []
 player_2_rects = []
 
-player_1_color = []
-player_2_color = []
-
 player_1_x = 0
 player_1_y = 300
 
 player_2_x = 0
 player_2_y = 500
+
+p1_red = False
+p2_red = False
+
+p1_score = 0
+p2_score = 0
 
 rects_margin = 80
 
@@ -38,21 +41,16 @@ win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("prisoner's dilemma")
 
 def allways_pos():
-    global player_1_x, p1_color
+    global player_1_x
     p1_template = pygame.Rect(player_1_x, player_1_y, 40, 40)
     player_1_x += rects_margin
     player_1_rects.append(p1_template)
-    p1_color = GREEN
-    player_1_color.append(p1_color)
-    print(player_1_color)
 
 def allways_neg():
-    global player_2_x, p2_color
+    global player_2_x
     p2_template = pygame.Rect(player_2_x, player_2_y, 40, 40)
     player_2_x += rects_margin
     player_2_rects.append(p2_template)
-    p2_color = RED
-    player_2_color.append(p2_color)
 
 running = True
 while running:
@@ -62,7 +60,7 @@ while running:
     
     win.fill(BLACK)
 
-    player_1_header = (font.render("Player 1:", True, WHITE))
+    player_1_header = (font.render("Player 1:" + str(p1_score), True, WHITE))
     player_1_header_rect = player_1_header.get_rect()
     win.blit(player_1_header, player_1_header_rect)
 
@@ -70,17 +68,15 @@ while running:
     player_2_header_rect = player_2_header.get_rect()
     player_2_header_rect.topleft = (650, 0)
     win.blit(player_2_header, player_2_header_rect)
-    
 
-    #for rect, color in player_1_rects:
-    #   count = 0
-    #   pygame.draw.rect(win, (p1_color[count]), rect)
-    #   count += 1
+
     for rect in player_1_rects:
         pygame.draw.rect(win, GREEN, rect)
+        p1_red = False 
     
     for rect in player_2_rects:
         pygame.draw.rect(win, RED, rect)
+        p2_red = True
 
     if runs > 0:
         allways_pos()
@@ -90,7 +86,16 @@ while running:
         runs -= 1
 
     ## Punkte vergabe
-    #for r1c, r2c in zip(player_1_rects, player_2_rects):
+    if p1_red == False and p2_red == False:
+        p1_score += 3
+        p2_score += 3
+    if p1_red == False and p2_red:
+        p2_score += 5
+    if p1_red and p2_red == False:
+        p1_score += 5
+    if p1_red and p2_red:
+        p1_score += 1
+        p2_score += 1
         
     
     pygame.display.flip()
