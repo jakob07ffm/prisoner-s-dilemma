9e3eb6c280c3a50f9d32e50f8c0bd57c1c5f20e4
Jakob__init__ <jsuetfels@gmail.com>
2024-08-23 12:49:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ win_height = 800
 rounds = 10
 runs = 0
 
-#Var die zeigt ob rot grün(kooperieren oder konkurenz)
-# 0 = konkurieren, 1 = kooperieren
 p1 = None
 p2 = None
 
@@ -31,33 +29,37 @@ p2_decisions = []
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("prisoner's dilemma")
 
-def p1_choise():
-    p1 = 0
+def p1_choice():
+    global p1
+    p1 = 0 
     p1_decisions.append(p1)
 
-def p2_choise():
-    p2 = 1
+def p2_choice():
+    global p2
     if runs >= 1:
-        if p1_decisions[runs] == 0:
-            p2 = 0
+
+        if p1_decisions[runs - 1] == 0:
+            p2 = 0 
+        else:
+            p2 = 1  
+    else:
+        p2 = 1 
     p2_decisions.append(p2)
 
 def points():
     global p1_score, p2_score
-    # Funktion die die Punkte vergibt:
 
-    #beide Partein kooperieren
+
+
     if p1_decisions[runs] == 1 and p2_decisions[runs] == 1:
         p1_score += 3
         p2_score += 3
 
-    #einer der Beiden kooperiert und der andere nicht
     if p1_decisions[runs] == 1 and p2_decisions[runs] == 0:
         p2_score += 5
-    if p2_decisions[runs] == 1 and p1_decisions[runs]== 0:
+    if p2_decisions[runs] == 1 and p1_decisions[runs] == 0:
         p1_score += 5
 
-    #beide Partein konkurieren
     if p1_decisions[runs] == 0 and p2_decisions[runs] == 0:
         p1_score += 1
         p2_score += 1
@@ -70,8 +72,8 @@ while running:
     
     win.fill(BLACK)
 
-    p1_choise()
-    p2_choise()
+    p1_choice()
+    p2_choice()
     points()
 
     print(runs)
@@ -87,14 +89,3 @@ while running:
 
 pygame.quit()
 sys.exit()
-
-
-# 1 = Green
-# 0 = Red
-
-
-#todo
-# - vielleicht return statement benutzen
-# - Neue Syteme / Algorüthmen finden + ihre Namen
-# - Das Belohnungssystem erstelle(score) #
-# - Gerade handel p2 mit dem wissen was p1 gewählt hat.#
